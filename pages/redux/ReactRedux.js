f828bbf14f4486ad00692a9ef4464e46ab34a0e2
zhugexinxin <zhugyx@gmail.com>
2021-10-28 22:07:48 +0800

    fix: style

--- pages/redux/ReactRedux.js
+++ pages/redux/ReactRedux.js
@@ -3,66 +3,66 @@ import React from "react"
 export const ReactContext = React.createContext({});
 
 export class Provider extends React.Component {
-	render() {
-		const { store, children } = this.props
+  render() {
+    const { store, children } = this.props
 
-		return (
-			<ReactContext.Provider value={store}>
-				{children}
-			</ReactContext.Provider>
-		)
-	}
+    return (
+      <ReactContext.Provider value={store}>
+        {children}
+      </ReactContext.Provider>
+    )
+  }
 }
 
 class ConnectComponent extends React.Component {
-	state = {
-		props: {}
-	}
+  state = {
+    props: {}
+  }
 
-	componentDidMount(){
-		const { store } = this.props
-		store.subscribe(() => this.update())
-		this.update()
-	}
+  componentDidMount(){
+    const { store } = this.props
+    store.subscribe(() => this.update())
+    this.update()
+  }
 
-	update(){
-		const { store, mapStateToProps, mapDispatchToProps, WrapComponent } = this.props
-		const stateProps = mapStateToProps(store.getState())
-		const dispatchProps = bindActionCreators(mapDispatchToProps, store.dispatch)
-		this.setState({
-			props:{
-				...this.state.props,
-				...stateProps,
-				...dispatchProps
-			}
-		})
-	}
+  update(){
+    const { store, mapStateToProps, mapDispatchToProps, WrapComponent } = this.props
+    const stateProps = mapStateToProps(store.getState())
+    const dispatchProps = bindActionCreators(mapDispatchToProps, store.dispatch)
+    this.setState({
+      props:{
+        ...this.state.props,
+        ...stateProps,
+        ...dispatchProps
+      }
+    })
+  }
 
-	render(){
-		const { WrapComponent } = this.props
+  render(){
+    const { WrapComponent } = this.props
 
-		return (
-			<ReactContext.Consumer>
-				<WrapComponent {...this.state.props} />
-			</ReactContext.Consumer>
-		)
-	}
+    return (
+      <ReactContext.Consumer>
+        <WrapComponent {...this.state.props} />
+      </ReactContext.Consumer>
+    )
+  }
 }
 
 export const connect = (
-	mapStateToProps = state => state,
-	mapDispatchToProps = {}
+  mapStateToProps = state => state,
+  mapDispatchToProps = {}
 )=>(WrapComponent)=>{
-	return (
-		<ReactContext.Consumer>
-			{(value) => (
-				<ConnectComponent
-					{...value}
-					mapStateToProps={mapStateToProps}
-					mapDispatchToProps={mapDispatchToProps}
-					WrapComponent={WrapComponent}
-				/>
-			)}
-		</ReactContext.Consumer>
-	)
+  return (
+    <ReactContext.Consumer>
+      {(value) => (
+        <ConnectComponent
+          {...value}
+          mapStateToProps={mapStateToProps}
+          mapDispatchToProps={mapDispatchToProps}
+          WrapComponent={WrapComponent}
+        />
+      )}
+    </ReactContext.Consumer>
+  )
 }
